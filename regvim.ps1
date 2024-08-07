f4538f546b5c71fd0337b1fedd5c213c8fbc5d93
Patrick Goodwin <pattygsocials@gmail.com>
2024-08-07 16:08:49 -0400

    Create regvim.ps1

---
+++ regvim.ps1
@@ -0,0 +1,20 @@
+function regvim {
+    param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$f 
+    )
+    if ([string]::IsNullOrEmpty($f)) {
+        Write-Error "Please provide a file name to search for."
+        return
+    }
+    $currentDirectory = Get-Location
+    $files = [System.IO.Directory]::EnumerateFiles($currentDirectory, $f, [System.IO.SearchOption]::AllDirectories) | ForEach-Object { $_ }
+    if ($files.Count -eq 0) {
+        Write-Error "No instances of '$f' found in subdirectories."
+        return
+    }
+    foreach ($file in $files) {
+        Write-Host("PowerShell (regvim): Opening file in vim: $file") -ForegroundColor Yellow
+        & "vim" $file
+    }
+}
